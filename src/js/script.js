485013679a0613acfb5c8a001e4a3439cd20b188
Artyom <76749602+MrArtga03@users.noreply.github.com>
2021-06-09 10:47:59 +0300

    initial commit

--- src/js/script.js
+++ src/js/script.js
@@ -6,18 +6,18 @@ likesEl.innerText = "No likes yet";
 
 let counter = 0;
 post.addEventListener("dblclick", () => {
-    counter++;
+  counter++;
 
-    if (counter === 1) {
-        likesEl.innerText = counter + " like";
-    } else {
-        likesEl.innerText = counter + " likes";
-    }
+  if (counter === 1) {
+    likesEl.innerText = counter + " like";
+  } else {
+    likesEl.innerText = counter + " likes";
+  }
 
-    heart.classList.add("animate-like");
-    setTimeout (() => {
-        heart.classList.remove("animate-like");
-    }, 800);
+  heart.classList.add("animate-like");
+  setTimeout(() => {
+    heart.classList.remove("animate-like");
+  }, 800);
 });
 
 //Info
@@ -25,4 +25,17 @@ post.addEventListener("dblclick", () => {
 const info = document.querySelector(".container-info");
 const btnInfo = document.querySelector(".icon-info");
 
-btnInfo.addEventListener("click", () => { if (btnInfo) info.style.display = "block"; });
+btnInfo.onmouseover = () => {
+  if (btnInfo) info.style.display = "block";
+};
+btnInfo.onmouseout = () => {
+  if (btnInfo) info.style.display = "none";
+};
+
+//display info while the cursor is on the info tab
+info.onmouseover = () => {
+  if (info) info.style.display = "block";
+};
+info.onmouseout = () => {
+  if (info) info.style.display = "none";
+};
